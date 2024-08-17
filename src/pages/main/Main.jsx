7a37ca163d8555efda411ff3d6ec7d750d8c18ac
Euntae Ki <rldmsxo@gmail.com>
2024-08-17 12:13:53 -0700

    Light Disappear WIP

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 import "./Main.css";
 import Light from "../../components/light/Light";
-import usePreviousLocation from '../../contexts/usePreviousLocation';
+import usePreviousLocation from "../../contexts/usePreviousLocation";
 
 const Main = ({
   pageColor,
@@ -13,28 +14,30 @@ const Main = ({
   setShowOverlay,
 }) => {
   const tree = `${process.env.PUBLIC_URL}/img/main_tree.png`;
+  const location = useLocation();
   const previousLocation = usePreviousLocation();
 
   // Figure out why the Main is calling PreviousLocation 3 times
   useEffect(() => {
     setPageColor("#FFFFFF");
-    // console.log(triggerBlink, previousLocation)
-    console.log('Main component render');
-    console.log('Trigger Blink:', triggerBlink);
-    console.log('Previous Location:', previousLocation);
-  }, [triggerBlink, setPageColor, previousLocation])
+  }, [location, previousLocation, triggerBlink, setPageColor]);
 
   return (
     <>
       <Light
-        display={triggerBlink && previousLocation && previousLocation.pathname !== "/"}
+        display={
+          // Make it disappear once it's done
+          (triggerBlink || location.pathname === "/home") &&
+          previousLocation &&
+          previousLocation.pathname !== "/"
+        }
         pageColor={pageColor}
         triggerBlink={triggerBlink}
         setTriggerBlink={setTriggerBlink}
         setPageColor={setPageColor}
         setShowOverlay={setShowOverlay}
         setOverlayColor={setOverlayColor}
-        slideType="up"
+        slideType={triggerBlink ? "down" : "up"}
       />
       <div id="main-container">
         <span id="main-description">
